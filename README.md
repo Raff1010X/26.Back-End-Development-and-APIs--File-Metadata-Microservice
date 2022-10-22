dea50568403365e5898e58a9fb39896f72c4d6b0
Rafał Kowalczyk <raff1410X@gmail.com>
2022-10-22 17:35:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# freeCodeCamp Back End Development and APIs certification
+
+## File Metadata Microservice project
+
+Working example: https://raff1010x.github.io/26.Back-End-Development-and-APIs--File-Metadata-Microservice/
+
+My git repo: https://github.com/Raff1010X/01.Roadmap
---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+
+require('dotenv').config();
+
+const app = express();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+const fileanalyse = require('./routes/fileanalyseRoutes');
+
+app.use('/api', upload.single('upfile'), fileanalyse);
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log('Your app is listening on port ' + port);
+});
